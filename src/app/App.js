da763718aa442a11952d2400df61dd2130fa9783
Emmy Steven <hello@emmysteven.com>
2021-04-09 16:33:42 +0100

    🎉 first commit

---
+++ src/app/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+export function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
