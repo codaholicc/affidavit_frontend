1fedc56c1310115826ace181d0fc90a97f47b867
Emmy Steven <hello@emmysteven.com>
2021-04-17 23:02:29 +0100

    chore: refactor import statements

--- src/modules/views/ProductCTA.js
+++ src/modules/views/ProductCTA.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Hidden from '@material-ui/core/Hidden';
-import Container from '@material-ui/core/Container';
+import { Grid, Hidden, Container, withStyles } from '@material-ui/core';
 import Typography from '../components/Typography';
 import TextField from '../components/TextField';
 import Snackbar from '../components/Snackbar';
